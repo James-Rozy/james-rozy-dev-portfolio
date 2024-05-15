954e449a01b95260abbffb3348b563e0223062aa
James Rozsypal <jimmerxl@gmail.com>
2024-05-15 12:20:16 -0400

    Add base React Router logic for the different page paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import About from './pages/About.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
+import Contact from './pages/Contact.jsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/about',
+		element: <About />,
+	},
+	{
+		path: '/portfolio',
+		element: <Portfolio />,
+	},
+	{
+		path: '/resume',
+		element: <Resume />,
+	},
+	{
+		path: '/contact',
+		element: <Contact />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
