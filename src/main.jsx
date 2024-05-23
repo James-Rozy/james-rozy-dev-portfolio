6ad87f6252537e104c8847f9b224240fc7cf98fb
James Rozsypal <jimmerxl@gmail.com>
2024-05-23 09:24:02 -0400

    Update page routing with seperate routes file

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx';
-import About from './pages/About.jsx';
-import Portfolio from './pages/Portfolio.jsx';
-import Resume from './pages/Resume.jsx';
-import Contact from './pages/Contact.jsx';
+import routes from './routes.jsx';
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-	},
-	{
-		path: '/about',
-		element: <About />,
-	},
-	{
-		path: '/portfolio',
-		element: <Portfolio />,
-	},
-	{
-		path: '/resume',
-		element: <Resume />,
-	},
-	{
-		path: '/contact',
-		element: <Contact />,
-	},
-]);
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
---
+++ src/routes.jsx
@@ -0,0 +1,32 @@
+import App from './App.jsx';
+import ErrorPage from './components/pages/ErrorPage.jsx';
+import About from './components/pages/About.jsx';
+import Portfolio from './components/pages/Portfolio.jsx';
+import Resume from './components/pages/Resume.jsx';
+import Contact from './components/pages/Contact.jsx';
+
+const routes = [
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: 'about',
+		element: <About />,
+	},
+	{
+		path: 'portfolio',
+		element: <Portfolio />,
+	},
+	{
+		path: 'resume',
+		element: <Resume />,
+	},
+	{
+		path: 'contact',
+		element: <Contact />,
+	},
+];
+
+export default routes;
