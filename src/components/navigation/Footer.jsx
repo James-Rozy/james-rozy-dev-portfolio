7c13ed4bd9f7a723bb76c7eb0363450ce05e2577
James Rozsypal <jimmerxl@gmail.com>
2024-05-28 13:47:06 -0400

    Create Footer element

---
+++ src/components/navigation/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+	return (
+		<div id='page-footer'>
+			<div className='footer-info'>
+				<p>James Rozsypal © 2024</p>
+			</div>
+			<div className='footer-contact'></div>
+		</div>
+	);
+};
+
+export default Footer;
